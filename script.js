b5441556c0ac64c097bedb3e2e947f976b01e1c7
DIVYANSHU_INDIA <mail.divyanshuupadhyay@gmail.com>
2023-09-07 11:22:48 +0530

    Adding the code

---
+++ script.js
@@ -0,0 +1,212 @@
+// JavaScript to handle page navigation
+const searchLink = document.getElementById('search-link');
+const favoritesLink = document.getElementById('favorites-link');
+const searchPage = document.getElementById('search-page');
+const favoritesPage = document.getElementById('favorites-page');
+// Adding click Event listner at search link button
+searchLink.addEventListener('click', () => {
+    searchPage.classList.add('active');
+    favoritesPage.classList.remove('active');
+});
+
+favoritesLink.addEventListener('click', () => {
+    favoritesPage.classList.add('active');
+    searchPage.classList.remove('active');
+});
+
+// JavaScript for meal search and favorite functionality
+const searchInput = document.getElementById('search-input');
+const searchResults = document.getElementById('search-results');
+const favoritesList = document.getElementById('favorites-list');
+const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const SUGGESTION_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+
+// Load favorites from localStorage
+const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+storedFavorites.forEach((meal) => {
+    addToFavorites(meal);
+});
+
+// Autocomplete suggestion
+searchInput.addEventListener('input', handleInput);
+
+function handleInput() {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm === '') {
+        searchResults.innerHTML = '';
+        return;
+    }
+
+    fetch(SUGGESTION_URL + searchTerm[0])
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.meals) {
+                displaySuggestionResults(data.meals);
+            } else {
+                searchResults.innerHTML = '';
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+            searchResults.innerHTML = '';
+        });
+}
+//function to display the suggestion
+function displaySuggestionResults(meals) {
+    searchResults.innerHTML = '';
+
+    meals.forEach((meal) => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion-item');
+        suggestionItem.textContent = meal.strMeal;
+        suggestionItem.addEventListener('click', () => {
+            searchInput.value = meal.strMeal;
+            searchResults.innerHTML = '';
+            handleFormSubmit(event);
+        });
+
+        searchResults.appendChild(suggestionItem);
+    });
+}
+// Function to search form
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the form from submitting traditionally
+
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm === '') {
+        searchResults.innerHTML = '';
+        return;
+    }
+// fecth function used here to fetch from the API
+    fetch(API_URL + searchTerm)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.meals) {
+                displayMealResults(data.meals);
+            } else {
+                searchResults.innerHTML = 'No meals found.';
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+            searchResults.innerHTML = 'Error fetching data. Please try again later.';
+        });
+}
+// Disp;ay function is used here to display the meal results
+function displayMealResults(meals) {
+    searchResults.innerHTML = '';
+
+    meals.forEach((meal) => {
+        const mealCard = document.createElement('div');
+        mealCard.classList.add('meal-card');
+
+        const mealName = document.createElement('h2');
+        mealName.textContent = meal.strMeal;
+
+        const mealImage = document.createElement('img');
+        mealImage.src = meal.strMealThumb;
+        mealImage.alt = meal.strMeal;
+
+        const idMeal = document.createElement('p');
+        idMeal.textContent = `Meal ID: ${meal.idMeal}`;
+
+        const strMeal = document.createElement('p');
+        strMeal.textContent = `Meal Name: ${meal.strMeal}`;
+
+        const strDrinkAlternate = document.createElement('p');
+        strDrinkAlternate.textContent = `If Alternate Drink required: ${meal.strDrinkAlternate}`;
+
+        const strCategory = document.createElement('p');
+        strCategory.textContent = `Category of Meal: ${meal.strCategory}`;
+
+        const strArea = document.createElement('p');
+        strArea.textContent = `Meal from: ${meal.strArea}`;
+
+        const strTags = document.createElement('p');
+        strTags.textContent = `Tags: ${meal.strTags}`;
+
+        const mealInstructions = document.createElement('p');
+
+        const showInstruction = document.createElement('button');
+        showInstruction.textContent = 'Show Instruction';
+        showInstruction.addEventListener('click', () => {
+            mealInstructions.textContent = `${meal.strInstructions}`;
+            showInstruction.hidden = true; // Disable the button after adding to favorites
+        });
+
+        const addToFavoritesButton = document.createElement('button');
+        addToFavoritesButton.textContent = 'Add to Fav';
+        addToFavoritesButton.addEventListener('click', () => {
+            addToFavorites(meal);
+            addToFavoritesButton.disabled = true; // Disable the button after adding to favorites
+        });
+        addToFavoritesButton.style.marginLeft = '50px';
+        mealCard.appendChild(mealName);
+        mealCard.appendChild(mealImage);
+        mealCard.appendChild(idMeal);
+        mealCard.appendChild(strMeal);
+        mealCard.appendChild(strDrinkAlternate);
+        mealCard.appendChild(strCategory);
+        mealCard.appendChild(strArea);
+        mealCard.appendChild(strTags);
+        mealCard.appendChild(mealInstructions);
+        mealCard.appendChild(showInstruction);
+        mealCard.appendChild(addToFavoritesButton);
+        searchResults.appendChild(mealCard);
+    });
+}
+
+function addToFavorites(meal) {
+    const listItem = document.createElement('div');
+    listItem.classList.add('favorite-item');
+    const idMeal = document.createElement('p');
+    idMeal.textContent = `Meal ID: ${meal.idMeal}`;
+    const mealName = document.createElement('h2');
+    mealName.textContent = meal.strMeal;
+    const strMeal = document.createElement('p');
+    strMeal.textContent = `Meal Name: ${meal.strMeal}`;
+    const strDrinkAlternate = document.createElement('p');
+    strDrinkAlternate.textContent = `If Alternate Drink required: ${meal.strDrinkAlternate}`;
+    const strCategory = document.createElement('p');
+    strCategory.textContent = `Category of Meal: ${meal.strCategory}`;
+    const removeFromFavoritesButton = document.createElement('button');
+    removeFromFavoritesButton.textContent = 'Remove from Fav';
+    removeFromFavoritesButton.addEventListener('click', () => {
+        removeFromFavorites(listItem);
+    });
+
+    listItem.appendChild(idMeal);
+    listItem.appendChild(mealName);
+    listItem.appendChild(strMeal);
+    listItem.appendChild(strDrinkAlternate);
+    listItem.appendChild(strCategory);
+    listItem.appendChild(removeFromFavoritesButton);
+    favoritesList.appendChild(listItem);
+
+    // Store favorites in localStorage
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    storedFavorites.push(meal);
+    localStorage.setItem('favorites', JSON.stringify(storedFavorites));
+}
+
+function removeFromFavorites(item) {
+    favoritesList.removeChild(item);
+
+    // Update favorites in localStorage
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const updatedFavorites = storedFavorites.filter((meal) => meal.strMeal !== item.firstChild.textContent);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+}
